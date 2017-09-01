8005c81670a480169140ee61df9d6a3867b1b0d0
Sue Lockwood <deathbearbrown@gmail.com>
2017-09-01 16:02:21 -0700

    poc

--- api/app.js
+++ api/app.js
@@ -23,9 +23,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/health', health);
 app.use('/docker', docker);
 app.use('/receiver', test);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,6 +40,12 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+// log the headers
+app.use(function(req, res, next){
+  console.log(req.headers);
+  next();
+})
 
 // error handler
 app.use(function(err, req, res, next) {
